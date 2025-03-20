70e6f4035a26ec83f3e4c1037531e11145fc523b
FrancoMelandri <franco.melandri@gmail.com>
2025-03-20 16:33:12 +0100

    introducing expression's Token to hold a value

---
+++ code/src/Token.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace BrainFp;
+
+public class Token<T>
+    where T: struct
+{
+    private T _data;
+
+    public Token(T data)
+    {
+        _data = data;
+    }
+    
+    public T Data => _data;
+}
---
+++ code/test/TokenTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Shouldly;
+
+namespace BrainFp.Test;
+
+public class TokenTests
+{
+    [Test]
+    public void Token_Int_Is_Right()
+    {
+        var token = new Token<int>(42);
+        token.Data.ShouldBe(42);
+    }
+
+    [Test]
+    public void Token_Double_Is_Right()
+    {
+        var token = new Token<Double>(42.0);
+        token.Data.ShouldBe(42.0);
+    }
+}
