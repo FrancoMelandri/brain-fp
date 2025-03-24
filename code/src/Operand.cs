2654b9383152eb28ddc8df2cc539afa1031afc31
franco-melandri-skanray <franco.melandri@skanray.com>
2025-03-24 16:58:46 +0100

    renaming Token() in Operand()

---
+++ code/src/Operand.cs
@@ -0,0 +1,44 @@
+﻿using TinyFp;
+
+namespace TinyBrain;
+
+public class Operand
+{
+    public (Option<Operand> Operand1, Option<Operand> Operand2) Previous { get; }
+    public double Data { get; }
+
+    public Operand()
+    {
+        Previous = (Option<Operand>.None(), Option<Operand>.None());
+    }
+
+    public Operand(double data)
+        : this()
+    {
+        Data = data;
+    }
+
+    private Operand(double data, (Operand, Operand) previous)
+        : this(data)
+    {
+        Previous = (Option<Operand>.Some(previous.Item1), Option<Operand>.Some(previous.Item2));
+    }
+
+    public static Operand operator +(Operand a, Operand b)
+        => new(a.Data + b.Data, (a, b));
+
+    public static Operand operator +(Operand a, double b)
+        => new(a.Data + b, (a, new Operand(b)));
+
+    public static Operand operator +(double a, Operand b)
+        => new(a + b.Data, (new Operand(a), b));
+
+    public static Operand operator *(Operand a, Operand b)
+        => new(a.Data * b.Data, (a, b));
+
+    public static Operand operator *(Operand a, double b)
+        => new(a.Data * b, (a, new Operand(b)));
+
+    public static Operand operator *(double a, Operand b)
+        => new(a * b.Data, (new Operand(a), b));
+}
---
+++ code/test/OperandTests.cs
@@ -0,0 +1,60 @@
+﻿using Shouldly;
+
+namespace TinyBrain.Test;
+
+public class OperandTests
+{
+    [Test]
+    public void Token_Sum_Is_Right()
+    {
+        var operand1 = new Operand(42.0);
+        var operand2 = new Operand(66.0);
+        var token = operand1 + operand2; 
+        token.Data.ShouldBe(108.0);
+        token.Previous.Operand1.Unwrap().ShouldBe(operand1);
+        token.Previous.Operand2.Unwrap().ShouldBe(operand2);
+    }
+
+    [Test]
+    public void Token_And_Constant_Sum_Is_Right()
+    {
+        var operand1 = new Operand(42.0);
+        var operand2 = operand1 + 10; 
+        var token3 = 10 + operand1; 
+        operand2.Data.ShouldBe(52.0);
+        token3.Data.ShouldBe(52.0);
+        operand2.Previous.Operand1.Unwrap().ShouldBe(operand1);
+        operand2.Previous.Operand2.Unwrap().Previous.Operand1.IsNone.ShouldBe(true);
+        operand2.Previous.Operand2.Unwrap().Previous.Operand2.IsNone.ShouldBe(true);
+        token3.Previous.Operand2.Unwrap().ShouldBe(operand1);
+        token3.Previous.Operand2.Unwrap().Previous.Operand1.IsNone.ShouldBe(true);
+        token3.Previous.Operand2.Unwrap().Previous.Operand2.IsNone.ShouldBe(true);
+    }
+
+    [Test]
+    public void Token_Mul_Is_Right()
+    {
+        var operand1 = new Operand(42.0);
+        var operand2 = new Operand(2.0);
+        var token = operand1 * operand2; 
+        token.Data.ShouldBe(84.0);
+        token.Previous.Operand1.Unwrap().ShouldBe(operand1);
+        token.Previous.Operand2.Unwrap().ShouldBe(operand2);
+    }
+
+    [Test]
+    public void Token_And_Constant_Mul_Is_Right()
+    {
+        var operand1 = new Operand(42.0);
+        var operand2 = operand1 * 2;
+        var token3 = 2 * operand1;
+        operand2.Data.ShouldBe(84.0);
+        token3.Data.ShouldBe(84.0);
+        operand2.Previous.Operand1.Unwrap().ShouldBe(operand1);
+        operand2.Previous.Operand2.Unwrap().Previous.Operand1.IsNone.ShouldBe(true);
+        operand2.Previous.Operand2.Unwrap().Previous.Operand2.IsNone.ShouldBe(true);
+        token3.Previous.Operand2.Unwrap().ShouldBe(operand1);
+        token3.Previous.Operand2.Unwrap().Previous.Operand1.IsNone.ShouldBe(true);
+        token3.Previous.Operand2.Unwrap().Previous.Operand2.IsNone.ShouldBe(true);
+    }
+}
